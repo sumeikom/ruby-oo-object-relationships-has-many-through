71d1b9ddfca9cdb1d0c811c5587a7931712c5ea0
sumeikom <sumeikom@gmail.com>
2021-03-21 21:43:59 -0400

    commit tree 624537f11e343b69cb132e57f714ff1e2557f2f7

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
+  attr_accessor :name, :experience
 
+  @@all = []
+
+  def initialize(name, experience)
+      @name = name
+      @experience = experience
+
+      @@all.push(self)
+  end
+
+  def self.all
+      @@all
+  end
+
+  def new_meal(customer, total, tip)
+      Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+      Meal.all.select {|meals| meals.waiter == self}
+  end
+
+  def best_tipper
+      tip = 0
+      Meal.all.each do |meals|
+          if meals.tip > tip
+              tip = meals.tip
+          end
+      end
+
+      Meal.all.find {|meals|
+      meals.tip == tip}.customer
+  end
 end
